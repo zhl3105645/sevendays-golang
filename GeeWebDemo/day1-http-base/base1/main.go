1e7318e065ef7a322a7f8812fc5b862bcc314a46
zhenghongliang.zhl <zhenghongliang.zhl@bytedance.com>
2022-03-11 16:55:49 +0800

    web day3 finish

---
+++ GeeWebDemo/day1-http-base/base1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandle)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+// handler echos r.URL.Path
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+// handler echos r.URL.Header
+func helloHandle(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+}
